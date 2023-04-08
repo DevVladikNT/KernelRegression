cded69580e53044b58e8cbbf4c0420689eab924d
Vladislav <trycatchernt@gmail.com>
2023-04-08 09:30:58 +0300

    eng translation and comments added

--- README.md
+++ README.md
@@ -1,15 +1,38 @@
 # NuclearRegression
 
-Моя реализация алгоритма построения ядерной регрессии на языке python.
+My realization of nuclear regression in python3
 
-## Цель
+## Goal
 
-Освоить современный метод фильтрации и построения непараметрической регрессии для решения задачи прогнозирования в задачах анализа больших данных, динамически снимаемых во времени.
+To master the modern method of filtering and constructing nonparametric regression to solve the forecasting problem in the analysis of big data dynamically captured in time.
 
-Требуется построить модель прогнозирования на основе реализации алгоритма непараметрической ядерной регрессии.
+It is required to build a prediction model based on the implementation of a nonparametric nuclear regression algorithm.
 
-## Исходные данные
+## Initial data
 
-В качестве исходных данных взята статистика заболеваемости людей от коронавируса в Санкт-Петербурге с начала марта 2020 года по конец июня 2020 года.
+As initial data, the statistics of the incidence of people from coronavirus in St. Petersburg from the beginning of March 2020 to the end of June 2020 were taken.
 
-Источник: [polymatica.ru](https://www.polymatica.ru/news/2020/dannye-po-koronavirusu-covid-19/?ysclid=lelcfa4b66884197378)
+Source: [polymatica.ru](https://www.polymatica.ru/news/2020/dannye-po-koronavirusu-covid-19/?ysclid=lelcfa4b66884197378)
+
+## Results
+
+First plot is the density of used core. In picture below you can see "Gaussian kernel".
+Some other cores are also available, but several aren't ploted (still works).
+
+![Core](pictures/core.png)
+
+There is visualization of distribution in second picture. It's hist and nuclear estimation of distribution density.
+
+![Distribution](pictures/distribution.png)
+
+Third one is result of nuclear regression.
+
+![Nuclear Regression](pictures/nuclear_regression.png)
+
+Picture below is my first try to make weighted nuclear regression. Result should be less affected by emissions, but now works bad (in my opinion, I'll try to fix it in future).
+
+![Weighted Nuclear Regression](pictures/weighted_nuclear_regression.png)
+
+And finally, I tried to predict 10% of the last elements. It looks better than usage of linear function and don't have edge effect when extrapolated by a polynomial. But nuclear regression is not suitable for extrapolation tasks.
+
+![Prediction](pictures/prediction.png)
--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from matplotlib import pyplot as plt
 from functools import lru_cache
 
 
-# Исходные данные по коронавирусу в СПб с 03.2020 по 06.2020
+# Initial data on coronavirus in St. Petersburg from 03.2020 to 06.2020
 x = np.arange(118)
-x = x / len(x)  # Приводим X к промежутку [0; 1] для корректной работы всех ядер
-# x = np.sort(np.random.random(118))
+x = x / len(x)  # We bring X to the interval [0; 1] for the correct operation of all cores
 y = np.array([0, 0, 1, 0, 1, 0, 0, 0, 3, 1, 0, 3, 0, 0, 1, 4, 2, 0,
               0, 5, 0, 5, 0, 11, 5, 8, 48, 27, 22, 9, 15, 20, 35, 69,
               34, 44, 35, 80, 69, 121, 121, 130, 154, 424, 139, 114,
@@ -17,22 +16,23 @@ y = np.array([0, 0, 1, 0, 1, 0, 0, 0, 3, 1, 0, 3, 0, 0, 1, 4, 2, 0,
               380, 375, 378, 347, 340, 326, 318, 313, 303, 274, 262,
               256, 252, 234, 228, 231, 228, 232, 229, 217, 220, 218,
               221, 223, 224, 217, 219, 253])
-# Тип ядра
-# 1 - Оптимальное (Епанечникова)
-# 2 - Квартичное
-# 3 - Треугольное
-# 4 - Прямоугольное
-# 5 - Гауссовское
-nuclear_type = 5
-gamma_nuclear_type = 5
-h = 0.1  # Размер окна
-predict = 0.9  # Какой % выборки будет использован для обучения предсказанию
+# Core type
+# 1 - Epanechnikov core
+# 2 - Quartic core
+# 3 - Triangle core
+# 4 - Rectangular core
+# 5 - Gaussian kernel
+# 6 - Cauchy core
+core_type = 5
+gamma_core_type = 6
+h = 0.1  # Window size
+predict = 0.9  # What % of the sample will be used for prediction training
 bins_count = 10
 
 
-# Гауссовское ядро
+# Returns core value
 @lru_cache()
-def nuclear(r, type_val=nuclear_type):
+def nuclear(r, type_val=core_type):
     if type_val == 1:
         return 3/4 * (1 - r ** 2)
     elif type_val == 2:
@@ -43,26 +43,28 @@ def nuclear(r, type_val=nuclear_type):
         return 1/2
     elif type_val == 5:
         return (2 * np.pi) ** (-1/2) * np.exp(-1/2 * r ** 2)
+    elif type_val == 6:
+        return 1 / (np.pi * (1 + r ** 2))
 
 
-# Расстояние от x1 до x2
+# Distance from x1 to x2
 @lru_cache()
 def distance(x1, x2):
     return np.abs(x1 - x2)
 
 
-# Значение ядерной регрессии в точке xi
+# The value of the nuclear regression at point xi
 def regression_result(xi, x_arr=x, y_arr=y, gamma_arr=np.ones(len(x))):
     upper_part = 0
     lower_part = 0
     for i in range(len(x_arr)):
-        k_val = nuclear(distance(xi, x_arr[i]) / h, nuclear_type)
+        k_val = nuclear(distance(xi, x_arr[i]) / h, core_type)
         upper_part += y_arr[i] * gamma_arr[i] * k_val
         lower_part += gamma_arr[i] * k_val
     return upper_part / lower_part
 
 
-# Среднеквадратичное отклонение реальных и вычисленных данных
+# Standard deviation of real and calculated data
 def error(regressed, actual):
     result = []
     for i in range(len(actual)):
@@ -73,19 +75,19 @@ def error(regressed, actual):
 def main():
     global h
 
-    # Выводим график, описывающий ядро
+    # We output a graph describing the core
     x_nuclear = np.arange(100)
     x_nuclear = x_nuclear / len(x_nuclear)
     y_nuclear = []
     for x_cur in x_nuclear:
         y_nuclear.append(nuclear(x_cur))
     plt.plot(x_nuclear, y_nuclear)
-    plt.title('Ядро')
+    plt.title('Core')
     plt.xlabel('r')
     plt.ylabel('F(r)')
     plt.show()
 
-    # Строим график плотности
+    # Building a density plot
     prob = []
     for xi in x:
         sum_i = 0
@@ -94,67 +96,67 @@ def main():
         sum_i /= len(x) * h
         prob.append(sum_i)
     plt.hist(x, bins=bins_count, edgecolor='black', label='Data histogram')
-    plt.plot(x, np.array(prob) * (len(x) / bins_count), label='Плотность распределения')
-    plt.title('Распределение')
+    plt.plot(x, np.array(prob) * (len(x) / bins_count), label='Distribution density')
+    plt.title('Distribution')
     plt.xlabel('x')
     plt.ylabel('count')
     plt.legend()
     plt.show()
 
-    # Выводим график, описывающий ядерную регрессию
+    # Building a plot describing the nuclear regression
     y_fact = []
     for i in range(len(x)):
         y_fact.append(regression_result(x[i]))
-    plt.plot(x, y_fact, label='Ядерная регрессия')
-    plt.plot(x, y, label='Реальные данные')
-    plt.title(f'Ядерная регрессия, h={h}')
+    plt.plot(x, y_fact, label='Nuclear regression')
+    plt.plot(x, y, label='Real data')
+    plt.title(f'Nuclear regression, h={h}')
     plt.xlabel('x')
     plt.ylabel('f(x)')
     plt.legend()
     plt.show()
 
-    # Вычисляем взвешенную ядерную регрессию
+    # Building a plot describing the weighted nuclear regression
     last_diff = 1000000
     gamma = np.ones(len(x))
     while True:
         y_fact = []
-        # Вычисляем значения с коэффициентами gamma
+        # We calculate values with gamma coefficients
         for i in range(len(x)):
             y_fact.append(regression_result(x[i], gamma_arr=gamma))
-        # Если ошибка выросла или стабилизировалась, выходим
+        # If the error has grown or stabilized, we exit
         if (diff := error(y_fact, y)) >= last_diff:
             print(diff)
             break
         print(diff)
         last_diff = diff
-        # Вычисляем новые значения коэффициентов gamma
+        # We calculate new values of gamma coefficients
         for i in range(len(y_fact)):
-            gamma[i] = nuclear(np.abs(y_fact[i] - y[i]), gamma_nuclear_type)
-    plt.plot(x, y_fact, label='Ядерная регрессия')
-    plt.plot(x, y, label='Реальные данные')
-    plt.title(f'Взвешенная ядерная регрессия, h={h}')
+            gamma[i] = nuclear(np.abs(y_fact[i] - y[i]), gamma_core_type)
+    plt.plot(x, y_fact, label='Nuclear regression')
+    plt.plot(x, y, label='Real data')
+    plt.title(f'Weighted nuclear regression, h={h}')
     plt.xlabel('x')
     plt.ylabel('f(x)')
     plt.legend()
     plt.show()
 
-    # Разбиваем выборку на тренировочную и тестовую
+    # We split the sample into training and test
     x_train = x[:int(predict * len(x))]
     y_train = y[:int(predict * len(y))]
     y_fact = []
-    # Вычисляем значения ядерной регресси на тренировочном диапазоне
+    # We calculate the values of nuclear regression in the training range
     for xi in x_train:
         y_fact.append(regression_result(xi, x_arr=x_train, y_arr=y_train))
     x_test = x[int(predict * len(x)):]
     y_test = []
-    # Вычисляем предсказание на тестовом диапазоне,
-    # используя ядерную регрессию, построенную на тренировочном диапазоне
+    # We calculate the prediction on the test range
+    # using a nuclear regression built on the training range
     for xi in x_test:
         y_test.append(regression_result(xi, x_arr=x_train, y_arr=y_train))
-    plt.plot(x_train, y_fact, label='Ядерная регрессия')
-    plt.plot(x_test, y_test, label='Предсказание')
-    plt.plot(x, y, label='Реальные данные')
-    plt.title(f'Предсказание ядерной регрессией, h={h}')
+    plt.plot(x_train, y_fact, label='Nuclear regression')
+    plt.plot(x_test, y_test, label='Prediction')
+    plt.plot(x, y, label='Real data')
+    plt.title(f'Prediction by nuclear regression, h={h}')
     plt.xlabel('x')
     plt.ylabel('f(x)')
     plt.legend()
